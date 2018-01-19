cccdd421612f043af6d60c611737a241982342bd
lager6 <lager6@gmail.de>
2018-01-19 13:42:01 +0100

    add upload form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+UPLOAD_FOLDER = '/static/img/process/'
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from app import app
+import os
 
 
 @app.route('/')
@@ -8,6 +9,17 @@ def index():
     return render_template('index.html', title='Photo-Reader', acHome='active')
 
 
-@app.route('/process')
+@app.route('/process', methods=['GET', 'POST'])
+
 def process():
-    return render_template('process.html', title='Process-Photo', acProcess='active')
+    foto_dir_path = "/static/img/"
+    fotoname = "upload_icon.png"
+    if request.method == "POST":
+        fotoname = request.form.get('foto', foto_dir_path)
+        file = request.files['foto']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], fotoname))
+    foto_path = foto_dir_path + fotoname
+    return render_template('process.html',
+                           title='Process-Photo',
+                           foto_path=foto_path,
+                           acProcess='active')
